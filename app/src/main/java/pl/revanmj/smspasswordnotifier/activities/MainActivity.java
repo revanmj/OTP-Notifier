f94fd174a2f29479fb29cfad7593ed3d60e68a15
Michał Jakubowski <revanmj@gmail.com>
2017-03-15 21:37:16 +0100

    Add requests for permissions

--- app/src/main/java/pl/revanmj/smspasswordnotifier/activities/MainActivity.java
+++ app/src/main/java/pl/revanmj/smspasswordnotifier/activities/MainActivity.java
@@ -1,5 +1,7 @@
 package pl.revanmj.smspasswordnotifier.activities;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -8,6 +10,9 @@ import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 
 import pl.revanmj.smspasswordnotifier.BuildConfig;
 import pl.revanmj.smspasswordnotifier.MessageProcessor;
@@ -15,6 +20,8 @@ import pl.revanmj.smspasswordnotifier.R;
 import pl.revanmj.smspasswordnotifier.data.SharedSettings;
 
 public class MainActivity extends AppCompatPreferenceActivity {
+
+    private static final int MY_PERMISSIONS_REQUEST_RECEIVE_SMS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,5 +72,77 @@ public class MainActivity extends AppCompatPreferenceActivity {
         if (!BuildConfig.DEBUG) {
             getPreferenceScreen().removePreference(testNoti);
         }
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECEIVE_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.RECEIVE_SMS)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+                showPermissionExplanaition();
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECEIVE_SMS},
+                        MY_PERMISSIONS_REQUEST_RECEIVE_SMS);
+
+                // MY_PERMISSIONS_REQUEST_RECEIVE_SMS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_RECEIVE_SMS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted,
+
+                } else {
+                    // permission denied
+                    showPermissionExplanaition();
+                }
+            }
+        }
+    }
+
+    public void showPermissionExplanaition() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle(R.string.title_no_permission);
+        builder.setMessage(R.string.message_app_no_permission);
+        builder.setPositiveButton(R.string.button_grant, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.RECEIVE_SMS},
+                        MY_PERMISSIONS_REQUEST_RECEIVE_SMS);
+            }
+        });
+        builder.setNegativeButton(R.string.button_exit, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+                MainActivity.this.finish();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
